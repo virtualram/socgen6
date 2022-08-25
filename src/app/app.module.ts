2e11cd3807add41fde607008f953bda576cbff0a
virtualram <ramnarayananrr@yahoo.com>
2022-08-25 10:32:10 +0530

    finalpush

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,29 @@ import { NgModel ,FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { Form1Component } from './form1/form1.component';
 import {HttpClient, HttpClientModule   } from '@angular/common/http';
 import { Service1Service } from './service1.service';
+import { MenuComponent } from './menu/menu.component';
+import { Routes, RouterModule ,ActivatedRoute } from '@angular/router';
+let  routes: Routes = [
+ {path: 'Form', component:Form1Component},
+
+{path:'App',component:AppComponent}]
 
 @NgModule({
   declarations: [
     AppComponent,
     Form1Component,
+    MenuComponent,
     
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,ReactiveFormsModule  ,  HttpClientModule
+    FormsModule,ReactiveFormsModule  ,  HttpClientModule,
+ RouterModule.forRoot(routes)
    
   ],
   providers: [Service1Service],
-  bootstrap: [AppComponent]
+  bootstrap: [MenuComponent]
 })
 export class AppModule { }
